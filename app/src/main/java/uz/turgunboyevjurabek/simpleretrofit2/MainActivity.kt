b1b6df5e1bfe98770190d4ba510428ec74f85ee7
Asus <turgunboyevjurabek777@gmail.com>
2024-01-31 15:31:39 +0500

    ui ga chiqmayapti :[

--- app/src/main/java/uz/turgunboyevjurabek/simpleretrofit2/MainActivity.kt
+++ app/src/main/java/uz/turgunboyevjurabek/simpleretrofit2/MainActivity.kt
@@ -1,23 +1,44 @@
 package uz.turgunboyevjurabek.simpleretrofit2
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.icu.text.LocaleDisplayNames.UiListItem
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import uz.turgunboyevjurabek.simpleretrofit2.madels.CatsFacts
 import uz.turgunboyevjurabek.simpleretrofit2.network.ApiClient
 import uz.turgunboyevjurabek.simpleretrofit2.ui.theme.SimpleRetrofit2Theme
 
@@ -31,35 +52,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                    List()
                 }
             }
         }
     }
 }
 
+@SuppressLint("MutableCollectionMutableState")
 @Composable
-fun Greeting() {
+ fun Greeting():ArrayList<CatsFacts> {
     val scope= rememberCoroutineScope()
     val context= LocalContext.current
-    LaunchedEffect(key1 = true){
-        scope.launch(Dispatchers.IO){
-            try {
-                val  data=ApiClient.api.getAllItem()
+    val data by remember { mutableStateOf(ArrayList<CatsFacts>()) }
 
-                withContext(Dispatchers.Main){
-                    Toast.makeText(context, "joningdan  $data", Toast.LENGTH_SHORT).show()
-                }
-            }catch (e:Exception){
-                withContext(Dispatchers.Main){
-                    Log.d("Iye", e.message.toString())
-                    Toast.makeText(context, "${e.message}", Toast.LENGTH_SHORT).show()
-                }
-
+    ApiClient.api.getAllItem().enqueue(object :Callback<CatsFacts>{
+        override fun onResponse(call: Call<CatsFacts>, response: Response<CatsFacts>) {
+            if (response.isSuccessful){
+                var list:CatsFacts = response.body()!!
+                data.addAll(listOf(list))
+                Toast.makeText(context, "keldi", Toast.LENGTH_SHORT).show()
             }
         }
-    }
 
+        override fun onFailure(call: Call<CatsFacts>, t: Throwable) {
+            Toast.makeText(context, "${t.message}", Toast.LENGTH_SHORT).show()
+        }
+    })
+
+   return data
 }
 
 @Preview(showSystemUi = true)
@@ -68,4 +89,27 @@ fun GreetingPreview() {
     SimpleRetrofit2Theme {
         Greeting()
     }
+}
+
+@Composable
+fun List() {
+    val list= Greeting()
+    Toast.makeText(LocalContext.current, "$list", Toast.LENGTH_SHORT).show()
+
+    LazyColumn(modifier = Modifier.fillMaxSize()){
+        items(list.size){
+            UiListItem(list[it],it)
+        }
+    }
+
+}
+
+@Composable
+fun UiListItem(catsFacts:CatsFacts,position:Int) {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 15.dp)){
+        Text(text = catsFacts.data[position].fact, fontSize = 18.sp, color = Color.Red)
+    }
+
 }
--- app/src/main/java/uz/turgunboyevjurabek/simpleretrofit2/network/ApiService.kt
+++ app/src/main/java/uz/turgunboyevjurabek/simpleretrofit2/network/ApiService.kt
@@ -1,13 +1,14 @@
 package uz.turgunboyevjurabek.simpleretrofit2.network
 
 import okhttp3.Response
+import retrofit2.Call
 import retrofit2.http.GET
 import uz.turgunboyevjurabek.simpleretrofit2.madels.CatsFacts
 import uz.turgunboyevjurabek.simpleretrofit2.madels.Data
 
 interface ApiService {
     @GET("facts/")
-    suspend fun getAllItem():CatsFacts
+     fun getAllItem():Call<CatsFacts>
 
 
 }
